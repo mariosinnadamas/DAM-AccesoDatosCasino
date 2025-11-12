cee03d3070ac893ed87f6b17bfa7e297e98ae41e
mariosinnadamas <mariosaeztellez@gmail.com>
2025-11-12 22:07:42 +0100

    Pruebo un Test de JUnit

---
+++ src/test/java/casino/ClienteTest.java
@@ -0,0 +1,21 @@
+package casino;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClienteTest {
+
+    Cliente c;
+
+    @Order(1)
+    @Test
+    void validarDni() {
+
+        Assertions.assertTrue(Cliente.validarDni("12345678Z"));
+        Assertions.assertTrue(Cliente.validarDni("12345678"));
+        Assertions.assertFalse(Cliente.validarDni("12345678"));
+    }
+}
